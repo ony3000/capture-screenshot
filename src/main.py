8cafd0dd1c09eb0fe4a3e5fdf882a8d32d657f5d
Hyeonjong <nianelo4@gmail.com>
2022-10-22 13:11:19 +0900

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,25 +1,59 @@
-import os
 import csv
+import os
 import time
+from dataclasses import dataclass
+
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
 
-now = time.strftime('%Y%m%d_%H%M%S')
-directory_name = f'screenshots_{now}'
 
-if not os.path.exists(directory_name):
-    os.makedirs(directory_name)
+@dataclass
+class SiteFormat:
+    name: str
+    url: str
 
-driver = webdriver.Firefox()
-driver.maximize_window()
 
-with open('./capture_list.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    for row in csv_reader:
-        name = row[0]
-        url = row[1]
-        driver.get(url)
-        time.sleep(1)
-        el = driver.find_element_by_tag_name('html')
-        el.screenshot(f'{directory_name}/{name}.png')
+def main() -> None:
+    now = time.strftime("%Y%m%d_%H%M%S")
+    base_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-driver.close()
+    with open(f"{base_path}/src/capture_list.csv") as csv_file:
+        valid_sites: list[SiteFormat] = []
+        csv_reader = csv.reader(csv_file, delimiter=",")
+
+        for row in csv_reader:
+            try:
+                name = row[0]
+                url = row[1]
+                valid_sites.append(SiteFormat(name=name, url=url))
+            except Exception as err:
+                pass
+
+        output_path = f"{base_path}/screenshots/{now}"
+
+        if not os.path.exists(output_path):
+            os.makedirs(output_path)
+
+        if len(valid_sites) == 0:
+            f = open(f"{output_path}/NO_VALID_SITES", "x")
+            f.close()
+            return
+
+        driver = webdriver.Chrome(
+            service=ChromeService(executable_path=ChromeDriverManager().install())
+        )
+        driver.maximize_window()
+
+        for site in valid_sites:
+            driver.get(site.url)
+            time.sleep(1)
+            el = driver.find_element(by=By.TAG_NAME, value="html")
+            el.screenshot(f"{output_path}/{site.name}.png")
+
+        driver.quit()
+
+
+if __name__ == "__main__":
+    main()
